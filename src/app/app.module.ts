b3cfea289c350c32e549c4da829ff6fc47d4e86b
Potapov <potapov.eo@gmail.com>
2023-03-27 14:11:54 +0300

    new folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../modules/material/material.module';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
