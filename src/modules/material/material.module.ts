ac9d081c069c376dedf066acdd348355bce51f78
Potapov <potapov.eo@gmail.com>
2023-03-27 21:58:05 +0300

    header

--- src/modules/material/material.module.ts
+++ src/modules/material/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon';
 
-
+const Modules : any[] = [
+  CommonModule,
+  MatToolbarModule,
+  MatIconModule
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: Modules,
+  exports: Modules
 })
 export class MaterialModule { }
